76ad2d2edcaee55de1d435153637d090625f93c6
zazu7765 <ohmyzazu@gmail.com>
2025-02-18 22:39:05 -0500

    feat(crucible): scrape course information

--- crucible/src/main.rs
+++ crucible/src/main.rs
@@ -6,5 +6,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let s = Scraper::new();
     let deps = s.get_departments("Faculty of Science")?;
     deps.iter().for_each(|dep| println!("{}", dep));
+    let cres = s.get_courses(deps.first().expect("EMPTY"))?;
+    cres.data
+        .iter()
+        .for_each(|course| println!("Name: {}\n", course.long_title));
     Ok(())
 }
--- crucible/src/scraper.rs
+++ crucible/src/scraper.rs
@@ -1,3 +1,5 @@
+use std::borrow::Borrow;
+
 const BASE_URL: &str = "https://www.torontomu.ca";
 const COURSE_PATH: &str = "/calendar/2024-2025/courses";
 
@@ -13,21 +15,23 @@ pub mod data {
     use serde::{Deserialize, Serialize};
 
     #[derive(Debug, Serialize, Deserialize)]
-    struct CourseResponse {
+    #[serde(rename_all = "camelCase")]
+    pub struct CourseResponse {
         hits_per_page: i32,   // Always 2000
         total_matches: i32,   // Varies by department/query
         original_offset: i32, // Always 0 in examples
-        data: Vec<Course>,
+        pub data: Vec<Course>,
     }
 
     #[derive(Debug, Serialize, Deserialize)]
     #[serde(rename_all = "camelCase")]
-    struct Course {
+    pub struct Course {
         // Required fields - present in all courses
         page: String, // e.g., "/content/ryerson/calendar/2024-2025/courses/physics/PCS/102"
-        data_url: String, // Links to JSON data
+        #[serde(default)]
+        data_url: Option<String>, // Links to JSON data
         course_code: String, // e.g., "PCS 102", "MTH 40A/B"
-        long_title: String, // Course title
+        pub long_title: String, // Course title
         course_description: String, // HTML-formatted description
         gpa_weight: String, // e.g., "1.00", "2.00", "0.00"
 
@@ -101,5 +105,28 @@ impl Scraper {
             })
             .collect();
         Ok(departments)
+    }
+
+    pub fn get_courses(&self, department: &str) -> Result<data::CourseResponse, ScrapeError> {
+        let link = department.replace("/content/ryerson", "");
+        let url = format!(
+            "{}{}/jcr:content/content/rescalendarcoursestack.data.1.json",
+            BASE_URL, link
+        );
+        let res = self
+            .client
+            .get(&url)
+            .header("Accept", "application/json")
+            .header("Referer", format!("{}{}", BASE_URL, link))
+            .send()?;
+
+        if !res.status().is_success() {
+            return Err(ScrapeError::APIError(format!(
+                "Failed to fetch courses from {} with status code {}",
+                url,
+                res.status().as_str()
+            )));
+        }
+        Ok(res.json()?)
     }
 }
