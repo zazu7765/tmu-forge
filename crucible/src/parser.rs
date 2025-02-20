35730b494197cd658ec2e34950820bcdc9b5d93d
zazu7765 <ohmyzazu@gmail.com>
2025-02-20 00:14:41 -0500

    chore(crucible): add rest of tests + comments + refactor structure

--- crucible/src/parser.rs
+++ crucible/src/parser.rs
@@ -16,18 +16,15 @@ enum RequisiteGroup {
 
 fn course_code(input: &str) -> IResult<&str, String> {
     // Take 3 upper case letters
-    let (input, dep) = take_while_m_n(3, 3, |c: char| c.is_ascii_uppercase())(input)?;
-    // Take 1+ spaces
-    let (input, _) = opt(multispace1).parse(input)?;
+    let scan_dep = take_while_m_n(3, 3, |c: char| c.is_ascii_uppercase());
     // Take 3 digits
-    let (input, code) = take_while_m_n(3, 3, |c: char| c.is_ascii_digit())(input)?;
+    let scan_code = take_while_m_n(3, 3, |c: char| c.is_ascii_digit());
     // Throw away weird course code semantics
-    let (input, _) = opt(alt((
-        recognize((alpha1, char('/'), alpha1)),
-        recognize(alpha1),
-    )))
-    .parse(input)?;
-    // Return the course code
+    let ignore_extra = alt((recognize((alpha1, char('/'), alpha1)), recognize(alpha1)));
+
+    // Combine and Parse
+    let (input, (dep, _, code, _)) =
+        (scan_dep, opt(multispace1), scan_code, opt(ignore_extra)).parse(input)?;
     Ok((input, format!("{} {}", dep, code)))
 }
 
@@ -37,6 +34,11 @@ mod tests {
 
     #[test]
     fn test_course_code() {
+        assert!(course_code("ABC").is_err());
+        assert!(course_code("ABC12").is_err());
+        assert!(course_code("AB 123").is_err());
+        assert!(course_code("123 ABC").is_err());
+        assert!(course_code("abc 123").is_err());
         assert_eq!(course_code("MTH 101"), Ok(("", "MTH 101".to_string())));
         assert_eq!(course_code("PCS 102A"), Ok(("", "PCS 102".to_string())));
         assert_eq!(course_code("MTH 130A/B"), Ok(("", "MTH 130".to_string())));
